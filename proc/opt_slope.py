12952a29e92ef0bffa6bcb01b686ecdd8d864cef
nikolaims <n.m.smetanin@gmail.com>
2019-04-10 18:00:05 +0300

    opt slope

--- proc/opt_slope.py
+++ proc/opt_slope.py
@@ -8,7 +8,7 @@ from proc.settings import CHANNELS, MONTAGE
 from mne.viz import plot_topomap
 
 
-def get_slope(y, max_env=None, max_slope=np.inf):
+def get_slope(y, max_x, ini_x):
     k = np.arange(15)
     def fun(x):
         eta, beta, a = x
@@ -31,8 +31,8 @@ def get_slope(y, max_env=None, max_slope=np.inf):
         d_beta_a = np.sum(2*k*(eta+2*beta*(a*k+1) - y))
         return np.array([[d_eta2, d_eta_beta, d_eta_a], [d_eta_beta, d_beta2, d_beta_a], [d_eta_a, d_beta_a, d_a2]])
 
-    bounds = LinearConstraint(np.eye(3), [0,0,-1/len(k)], [max_env or np.inf, max_env or np.inf, max_slope])
-    res = minimize(fun, np.array([max_env or 1, max_env or 1, 0]), jac=jac, hess=hess, method='trust-constr', constraints=bounds)
+    bounds = LinearConstraint(np.eye(3), [0,0,-1/len(k)], max_x)
+    res = minimize(fun, ini_x, jac=jac, hess=hess, method='trust-constr', constraints=bounds)
     return res.x[2]
 
 
@@ -51,10 +51,17 @@ for f, (fb_type, fb_type_df) in enumerate(all_stats_df.groupby('fb_type')):
     for s, (subj_id, subj_df) in tqdm(enumerate(fb_type_df.groupby('subj_id')), fb_type):
         for c, (ch, ch_df) in enumerate(subj_df.groupby('channel')):
             curve = ch_df['metric'].values
-            slopes[f, s, c] = get_slope(curve, curve.max(), 0.2)
+            curve[np.isinf(curve)] = np.nan
+            curve = pd.Series(curve).fillna(method='bfill').values
+            slopes[f, s, c] = get_slope(curve, [np.max(curve), np.max(curve),  1/14], [np.mean(curve)/2, np.mean(curve)/2, 0])
+            #slopes[f, s, c] = linregress(np.arange(15), curve).slope
 
+
+
+vmax = 10
 fig, axes = plt.subplots(4, 11)
+pers = 0.5/14
 for f in range(4):
     for s in range(10):
-        plot_topomap(slopes[f, s], MONTAGE.get_pos(), axes=axes[f, s], vmin=-0.2, vmax=0.2)
-    plot_topomap(np.mean(slopes[f], 0), MONTAGE.get_pos(), axes=axes[f, -1], vmin=-0.2, vmax=0.2)
+        plot_topomap(slopes[f, s], MONTAGE.get_pos(), mask=slopes[f, s]>pers, axes=axes[f, s], cmap='viridis', vmin=0, vmax=np.percentile(slopes, 90), contours=0)
+    plot_topomap(np.mean(slopes[f], 0), MONTAGE.get_pos(), mask=np.mean(slopes[f], 0)>pers, axes=axes[f, -1], cmap='viridis', vmin=0, vmax=np.percentile(slopes, 90), contours=0)
