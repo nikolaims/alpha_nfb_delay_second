0fcfd3b85913a9724fa98b92b7078eaa0d3ec267
nikolaims <n.m.smetanin@gmail.com>
2019-04-29 13:09:57 +0300

    fixes

--- proc/multichannel/curves.py
+++ proc/multichannel/curves.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 import pandas as pd
 
 
-all_stats_df = pd.read_pickle('data/2split_metrics_chs_ica_all.pkl')
+all_stats_df = pd.read_pickle('data/multichannel_metrics_split_local.pkl')
 all_stats_df = all_stats_df.loc[all_stats_df['block_number']>1000]
 all_stats_df = all_stats_df.loc[all_stats_df['channel'].isin(CHANNELS)]
 all_stats_df = all_stats_df.loc[all_stats_df['threshold_factor'].isin([2])]
@@ -27,9 +27,28 @@ for metric_type, metric_type_df in all_stats_df.groupby('metric_type'):
             for c, (ch, ch_df) in enumerate(subj_df.groupby('channel')):
                 curve = ch_df['metric'].values
                 curve[np.isinf(curve)] = np.nan
-                curve = pd.Series(curve).fillna(method='bfill').fillna(method='ffill').values
-                curve[np.isnan(curve)] = 0.001
-                # curve = pd.Series(curve).rolling(3).median().fillna(method='bfill')
-                # if np.any(np.isnan(curve)): print(curve)
+                curve[np.isnan(curve)] = 0.0001
+
+                curve = sg.filtfilt(np.ones(3)/3, [1, 0], curve)
+                x0 = linregress(np.linspace(0, 1, 30), curve).intercept
+                #x0 = curve[:15].mean()
+                curve = curve/x0-1
                 y_df = y_df.append(pd.DataFrame({'metric_type':metric_type, 'fb_type': fb_type, 'subj_id': 's'+str(subj_id), 'channel': ch, 'k': np.linspace(0, 1, 30), 'env': curve+0.0001}), ignore_index=True)
+
+
+fig, axes = plt.subplots(4, 6, sharey='row', sharex='all')
+for m, (metric_type, metric_type_df) in enumerate(y_df.query('channel=="C3"').groupby('metric_type')):
+    for f, (fb_type, fb_type_df) in enumerate(metric_type_df.groupby('fb_type')):
+        curves = []
+        for s, (subj_id, subj_df) in enumerate(fb_type_df.groupby('subj_id')):
+            curve = subj_df['env'].values
+            axes[m, f].plot(curve)
+            axes[m, f].set_title('{}\n{}'.format(metric_type, fb_type))
+            curves.append(curve)
+        axes[m, f].plot(np.mean(curves, 0), 'k')
+        axes[m, f].plot(np.median(curves, 0), 'k--')
+        axes[m, 4].plot(np.mean(curves, 0))
+        axes[m, 4].fill_between(np.arange(30), np.mean(curves, 0) - np.std(curves, 0)/10**0.5, np.mean(curves, 0) + np.std(curves, 0)/10**0.5, alpha=0.2)
+        axes[m, 5].plot(np.median(curves, 0))
+        axes[m, 5].fill_between(np.arange(30), np.percentile(curves, 5, 0), np.percentile(curves, 95, 0), alpha=0.2)
 
--- proc/multichannel/permut/perm_all.py
+++ proc/multichannel/permut/perm_all.py
@@ -27,9 +27,10 @@ for metric_type, metric_type_df in all_stats_df.groupby('metric_type'):
             for c, (ch, ch_df) in enumerate(subj_df.groupby('channel')):
                 curve = ch_df['metric'].values
                 curve[np.isinf(curve)] = np.nan
-                curve = pd.Series(curve).fillna(method='bfill').fillna(method='ffill').values
-                curve[np.isnan(curve)] = 0.001
-                curve = pd.Series(curve).rolling(3).median().fillna(method='bfill')
+                curve[np.isnan(curve)] = 0.0001
+                #curve = sg.filtfilt([1/6, 1/3, 1/3, 1/6], [1, 0], curve)
+                x0 = linregress(np.linspace(0, 1, 30), curve).intercept
+                curve = curve/x0-1
                 #if np.any(np.isnan(curve)): print(curve)
                 y_df = y_df.append(pd.DataFrame({'metric_type':metric_type, 'fb_type': fb_type, 'subj_id': 's'+str(subj_id), 'channel': ch, 'k': np.linspace(0, 1, 30), 'env': curve+0.0001}), ignore_index=True)
 
@@ -96,7 +97,8 @@ for m, metric_type in enumerate(['magnitude', 'n_spindles', 'duration', 'amplitu
             def get_stat(y1, y2, fun=np.array):
                 stat = linregress(x, fun(y1.T.flatten())).slope - linregress(x, fun(y2.T.flatten())).slope
                 #stat = linregress(np.arange(30), (np.mean(y1, 0)-np.mean(y2, 0))).slope
-                #stat = np.sum((np.mean(y1, 0) - np.mean(y2, 0)))
+                #stat = np.sum((np.mean(y1, 0)[15:] - np.mean(y2, 0)[15:]))
+                #stat = np.mean(np.mean(y1[:, 15:], 1)/np.mean(y1[:, :15], 1)) - np.mean(np.mean(y2[:, 15:], 1)/np.mean(y2[:, :15], 1))
                 return stat
 
 
@@ -120,7 +122,7 @@ fig, axes = plt.subplots(4, 4)
 for m, metric_type in enumerate(['magnitude', 'n_spindles', 'duration', 'amplitude']):
     for f, fb1_type in tqdm(enumerate(['FB0', 'FB250', 'FB500', 'FBMock']), metric_type):
 
-        fdr_mask = fdr_correction(ch_p_vals[m, f], 0.05)[0]
+        fdr_mask = fdr_correction(ch_p_vals, 0.05)[0][m, f]
         im = plot_topomap(np.log10(ch_p_vals[m, f]+0.0001), MONTAGE.get_pos(), mask=fdr_mask, axes=axes[m, f], show=False, vmin=-2, vmax=0, cmap='inferno_r', contours=[np.log10(0.05)])
         axes[0, f].set_title(fb1_type)
 
@@ -136,44 +138,65 @@ cax.set_title('p-value')
 
 
 
+from scipy.stats import rankdata
+def rankstat(x, y):
+    s = np.array([np.sum(rankdata(np.concatenate((x[:, k], y[:, k])))[:len(x)]) for k in range(x.shape[1])])
+    return s
+
+
+n_observations = 10
+n_times = 30
+n_vertices = 32
+
+
+for metric_type in ['magnitude', 'n_spindles', 'duration', 'amplitude']:
+    for fb_type in ['FB0', 'FB250', 'FB500']:
+
+        g1 = np.swapaxes(np.array([[y_df.query('metric_type=="{}" & subj_id=="{}" & channel=="{}"'.format(metric_type, s, ch))['env'].values
+             for ch in CHANNELS] for s in y_df.query('fb_type=="{}"'.format(fb_type))['subj_id'].unique()]), 2,1)
+
+        g2 = np.swapaxes(np.array([[y_df.query('metric_type=="{}" & subj_id=="{}" & channel=="{}"'.format(metric_type, s, ch))['env'].values
+             for ch in CHANNELS] for s in y_df.query('fb_type=="FBMock"')['subj_id'].unique()]), 2,1)
+
+
+        from mne.stats import  spatio_temporal_cluster_test
+
+        from mne import create_info
+        from mne.channels import read_montage, find_ch_connectivity
+
+        cnk = find_ch_connectivity(create_info(CHANNELS, 250, 'eeg', read_montage('standard_1005')), 'eeg')[0]
+
+
+        t_obs, clusters, cluster_pv, h0 = spatio_temporal_cluster_test([g1, g2], 138, stat_fun=rankstat, tail=1, connectivity=cnk)
+        cluster_pv
+
+
+        good_cluster_inds = np.where(cluster_pv < 0.05)[0]
 
 
 
+        for i_clu, clu_idx in enumerate(good_cluster_inds[:10]):
+            # unpack cluster information, get unique indices
+            time_inds, space_inds = np.squeeze(clusters[clu_idx])
+            ch_inds = np.unique(space_inds)
+            time_inds = np.unique(time_inds)
+
+            # get topography for F stat
+            f_map = t_obs[time_inds, ...].mean(axis=0)
+
+            # get signals at the sensors contributing to the cluster
+            sig_times = np.arange(30)[time_inds]
+
+            # create spatial mask
+            mask = np.zeros((f_map.shape[0], 1), dtype=bool)
+            mask[ch_inds, :] = True
+
+            plt.figure()
+            plt.title('{}\n{}_{}'.format(metric_type, fb_type, i_clu))
+            plt.xlabel('{}-{}'.format(sig_times[0], sig_times[-1]))
+            plot_topomap(f_map, MONTAGE.get_pos(), mask=mask, cmap='inferno',
+                         vmin=105, vmax=139, show=True)
+            plt.savefig('data/img/clust/{}_{}_{}'.format(metric_type, fb_type, i_clu))
+            plt.close()
 
 
-
-fb2_type = 'FBMock'
-x = np.linspace(0, 1, 30).repeat(10)
-ch_p_vals = np.zeros((4, 4, len(CHANNELS)))
-
-n_perm = 1000
-ch_stats = np.zeros((4, 4, len(CHANNELS)))
-
-
-fig, axes = plt.subplots(4, 4, sharex=True, sharey=True)
-
-for m, metric_type in enumerate(['magnitude', 'n_spindles', 'duration', 'amplitude']):
-    for f, fb1_type in tqdm(enumerate(['FB0', 'FB250', 'FB500', 'FBMock']), metric_type):
-
-        fb1_subjs = y_df.query('fb_type=="{}"'.format(fb1_type))['subj_id'].unique()
-        fb2_subjs = y_df.query('fb_type=="{}"'.format(fb2_type))['subj_id'].unique()
-        all_subjs = np.concatenate([fb1_subjs, fb2_subjs])
-        y = np.array([[y_df.query('subj_id=="{}" & channel=="{}" & metric_type=="{}"'.format(s, ch, metric_type))['env'] for s in all_subjs] for ch in CHANNELS])
-
-
-        for c, ch in enumerate(CHANNELS):
-            #
-            # def get_stat(y1, y2, fun=np.array):
-            #     y1 = y1.T
-            #     y2 = y2.T
-            #     stat = linregress(x, fun(y1.flatten())).slope# - linregress(x, fun(y2.flatten())).slope
-            #     #stat = np.mean(np.mean(y1[15:], 0)/np.mean(y1[:15], 0)) - np.mean(np.mean(y2[15:], 0)/np.mean(y2[:15], 0))
-            #     return stat
-            lr = linregress(x, (y[c, :10] - y[c, 10:]).T.flatten())
-            ch_stats[m, f, c] = lr.slope
-            #ch_p_vals[m, f, c] = (n_perm-np.sum(stats>0))/n_perm
-            ch_p_vals[m, f, c] =  lr.pvalue
-            if ch =='P4':
-                axes[m, f].plot(np.linspace(0, 1, 30), y[c, :10].mean(0) - y[c, 10:].mean(0))
-
-from mne.stats import fdr_correction
