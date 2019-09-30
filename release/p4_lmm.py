2d0fdadb3d07bb0ebd589939bd56135c3981c206
nikolaims <n.m.smetanin@gmail.com>
2019-09-30 17:02:54 +0300

    p4 lmm

---
+++ release/p4_lmm.py
@@ -0,0 +1,75 @@
+from mne.stats import fdr_correction
+from pymer4.models import Lm, Lmer
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+stats_df = pd.read_pickle('release/data/{}.pkl'.format('channels1_bands1_splitedTrue_thresholds17'))
+stats_df = stats_df.loc[stats_df['block_number']> 1000]
+unique_blocks = list(stats_df['block_number'].unique())
+stats_df['k'] = stats_df['block_number'].apply(lambda x: unique_blocks.index(x))
+stats_df['subj_id_str'] = 's' + stats_df['subj_id'].astype('str')
+
+
+
+t_stat_df = pd.DataFrame(columns=['metric_type', 'threshold_factor', 'Contrast', 'T-stat'])
+
+
+for threshold_factor in stats_df.threshold_factor.unique():
+    for metric_type in ['magnitude', 'n_spindles', 'amplitude', 'duration']:
+        data = stats_df.query('metric_type=="{}" & threshold_factor=={}'.format(metric_type, threshold_factor)).copy()
+        print(len(data))
+        data = data.replace([np.inf, -np.inf], np.nan)
+        data.loc[:, 'metric'] = data['metric'].fillna(data['metric'].min()).values
+
+        model = Lmer('metric ~ k:fb_type + (1 |subj_id_str)', data=data)
+        model.fit(factors={'fb_type': ['FB0', 'FB250', 'FB500', 'FBMock']})
+        print('***', metric_type)
+        res = model.post_hoc('k', 'fb_type')[1][['Contrast', 'T-stat']]
+        res['metric_type'] = metric_type
+        res['threshold_factor'] = threshold_factor
+        t_stat_df = t_stat_df.append(res, ignore_index=True)
+
+
+
+import pylab as plt
+fig, axes = plt.subplots(3, 3, sharey='all', sharex='all', figsize=(6, 7))
+plt.subplots_adjust(wspace=0, hspace=0)
+for j_metric_type, metric_type in enumerate(['n_spindles', 'amplitude', 'duration']):
+    axes[0, j_metric_type].set_title(metric_type)
+    for contrast, color in zip(['FB0 - FBMock', 'FB250 - FBMock', 'FB500 - FBMock'], ['#6AC3E8', '#438BA8', '#005375']):
+        data = t_stat_df.query('Contrast=="{}" & metric_type=="{}"'.format(contrast, metric_type)).copy()
+        axes[0, j_metric_type].plot(data['T-stat'].values, data['threshold_factor'].values, color=color)
+        reject_fdr, pval_fdr = fdr_correction(stats.t.sf(data['T-stat'].values, 9), alpha=0.05, method='indep')
+        if len(data['T-stat'].values[reject_fdr]) > 0:
+            axes[0, j_metric_type].plot(data['T-stat'].values[reject_fdr], data['threshold_factor'].values[reject_fdr], '*', color=color, markersize=7, alpha=0.9)
+[ax.axvline(2.228, color='#FE4A49', linestyle='--') for ax in axes[0,:]]
+[ax.axvline(-2.228, color='#FE4A49', linestyle='--') for ax in axes[0,:]]
+
+
+for j_metric_type, metric_type in enumerate(['n_spindles', 'amplitude', 'duration']):
+    for contrast, color in zip(['FB0 - FB500', 'FB250 - FB500'], ['#6AC3E8', '#438BA8']):
+        data = t_stat_df.query('Contrast=="{}" & metric_type=="{}"'.format(contrast, metric_type)).copy()
+        axes[1, j_metric_type].plot(data['T-stat'].values, data['threshold_factor'].values, color=color)
+        reject_fdr, pval_fdr = fdr_correction(stats.t.sf(data['T-stat'].values, 9), alpha=0.10, method='indep')
+        if len(data['T-stat'].values[reject_fdr]) > 0:
+            axes[1, j_metric_type].plot(data['T-stat'].values[reject_fdr], data['threshold_factor'].values[reject_fdr], '*', color=color, markersize=7, alpha=0.9)
+[ax.axvline(2.228, color='#005375', linestyle='--') for ax in axes[1,:]]
+[ax.axvline(-2.228, color='#005375', linestyle='--') for ax in axes[1,:]]
+
+for j_metric_type, metric_type in enumerate(['n_spindles', 'amplitude', 'duration']):
+    for contrast, color in zip(['FB0 - FB250'], ['#6AC3E8']):
+        data = t_stat_df.query('Contrast=="{}" & metric_type=="{}"'.format(contrast, metric_type)).copy()
+        axes[2, j_metric_type].plot(data['T-stat'].values, data['threshold_factor'].values, color=color)
+        reject_fdr, pval_fdr = fdr_correction(stats.t.sf(data['T-stat'].values, 9), alpha=0.10, method='indep')
+        if len(data['T-stat'].values[reject_fdr]) > 0:
+            axes[2, j_metric_type].plot(data['T-stat'].values[reject_fdr], data['threshold_factor'].values[reject_fdr], '*', color=color, markersize=7, alpha=0.9)
+[ax.axvline(2.228, color='#438BA8', linestyle='--') for ax in axes[2,:]]
+[ax.axvline(-2.228, color='#438BA8', linestyle='--') for ax in axes[2,:]]
+
+axes[0,0].set_ylabel('FBMock\nThreshold')
+axes[1,0].set_ylabel('FB500\nThreshold')
+axes[2,0].set_ylabel('FB250\nThreshold')
+
+[ax.set_xlabel('T-stat') for ax in axes[2, :]]
+plt.savefig('lmm-tests.png', dpi=200)
