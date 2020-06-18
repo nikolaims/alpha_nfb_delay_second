642eb2f325015f284603a5e7b1a369a6df7de8bb
nikolaims <n.m.smetanin@gmail.com>
2020-06-18 18:44:58 +0300

    fan test

--- release/p4_fan98.py
+++ release/p4_fan98.py
@@ -1,7 +1,7 @@
 from release.settings import FB_ALL
 import pandas as pd
 import numpy as np
-from release.stats.fan98test import fan98test
+from release.stats.fan98test import fan98test, simulate_h0_distribution, get_p_val
 import pylab as plt
 
 stats_file = 'block_stats_1channels_1bands_median_20ths.pkl'
@@ -21,13 +21,23 @@ for ind, fb_type in enumerate(fb_types):
     data_points = np.zeros((len(unique_subj), len(FB_ALL)))
     for j, subj_id in enumerate(unique_subj):
         data_points[j, :] = fb_stats_df.query('subj_id=={}'.format(subj_id))['metric']
-        data_points[j, :] /= data_points[j, 0]
-        # data_points[j, :] /= data_points[j, :].mean()
-    fb_data_points.append(data_points[:, 1:])
+        # data_points[j, :] /= data_points[j, 0]
+        data_points[j, :] /= data_points[j, :].mean()
+    fb_data_points.append(data_points[:, :])
 
     plt.errorbar(np.arange(15)+ind*0.05, data_points.mean(0), 2*data_points.std(0)/np.sqrt(len(unique_subj)))
     # plt.plot(np.arange(15), data_points.T, color='C{}'.format(ind), alpha=0.5)
 
+n_subjects_list = sorted([fb_data_points[k].shape[0] for k in range(len(fb_data_points))])
+d_list = np.arange(sum(n_subjects_list[:2]) - 2, sum(n_subjects_list[-2:]) - 2 + 1)
+h0_distributions_dict = {}
+for d in d_list:
+    h0_distributions_dict[d] = simulate_h0_distribution(fb_data_points[0].shape[1], d)
+
+
 for ind1 in range(len(fb_types)):
     for ind2 in range(len(fb_types)-1, ind1, -1):
-        print(fb_types[ind1], fb_types[ind2], fan98test(fb_data_points[ind1], fb_data_points[ind2])[0])
+        stat = fan98test(fb_data_points[ind2], fb_data_points[ind1])
+        d = fb_data_points[ind1].shape[0] + fb_data_points[ind2].shape[0] - 2
+        p = get_p_val(stat, h0_distribution=h0_distributions_dict[d])
+        print(fb_types[ind1], fb_types[ind2], stat, p)
--- release/stats/fan98test.py
+++ release/stats/fan98test.py
@@ -1,7 +1,7 @@
 import numpy as np
+import os
 
-
-def fan98test(data_points1, data_points2, plot_steps=False, method='legendre'):
+def fan98test(data_points1, data_points2, plot_steps=False, transform='legendre'):
     # mean, std and number of samples
     x1_mean = np.mean(data_points1, 0)
     x1_std = np.std(data_points1, 0)
@@ -16,36 +16,38 @@ def fan98test(data_points1, data_points2, plot_steps=False, method='legendre'):
     # z - score over time(see formula 13)
     z_score = (x1_mean - x2_mean) / (x1_std ** 2 / x1_n + x2_std ** 2 / x2_n) ** 0.5
 
-    if method == 'legendre':
-        z_star = legendre_transform(z_score)
-    elif method == 'fft':
-        z_star = fourier_transform(z_score)
+    stat = eval_fan98_stat(z_score, d, transform)
+    # eval statistic for each number of first blocks (see fomula 6)
+
+    # chi2 statistic benchmark(see formula 2)
+    # chi2stat = np.sum(z_score**2)
+
+    return stat
+
+
+def eval_fan98_stat(z_star, d, transform):
+    if transform == 'legendre':
+        z_star = legendre_transform(z_star)
+    elif transform == 'fft':
+        z_star = fourier_transform(z_star)
+    elif transform is None or transform=='identity':
+        pass
     else:
         raise TypeError('Unknown method type')
 
-
-    # eval statistic for each number of first blocks (see fomula 6)
     T_an = np.zeros(len(z_star))
+    d_factor = 1 if d is None else ((d - 2) ** 2 * (d - 4) / (d ** 2 * (d - 1))) ** 0.5
     for m in range(len(z_star)):
-        T_an[m] = np.sum(z_star[:m+1]**2 - 1)*((d - 2)**2 * (d - 4) / (2 * (m+1) * d**2 * (d - 1)))**0.5
-
+        T_an[m] = np.sum(z_star[:m + 1] ** 2 - 1) * d_factor / (2 * (m + 1)) ** 0.5
 
     # find maximum T (see formula 6)
     stat = np.max(T_an)
 
-
-
     # compute final stat(see fomula 4)
-    loglogn = np.log(np.log(len(z_score)))
+    loglogn = np.log(np.log(len(z_star)))
 
-
-    stat = (2 * loglogn)**0.5 * stat - (2 * loglogn + 0.5 * np.log(loglogn) - 0.5 * np.log(4 *np.pi))
-
-    # chi2 statistic benchmark(see formula 2)
-    chi2stat = np.sum(z_score**2)
-
-    return stat, chi2stat
-
+    stat = (2 * loglogn) ** 0.5 * stat - (2 * loglogn + 0.5 * np.log(loglogn) - 0.5 * np.log(4 * np.pi))
+    return stat
 
 def fourier_transform(x):
     # fft (see formula between 17 and 18)
@@ -65,3 +67,52 @@ def legendre_transform(x):
         basis[:, k] = (a - np.mean(a))**k
     q, _ = np.linalg.qr(basis)
     return x.dot(q)
+
+
+def simulate_h0_distribution(n, d=None, n_iter=100000, transform='legendre'):
+    cash_dir = '_fan98_temp'
+    cash_file = os.path.join(cash_dir, 'h0_n{}_d{}_n_iter{}_{}.npy'.format(n, d, n_iter, transform))
+    if os.path.exists(cash_file):
+        print('Load from {}'.format(cash_file))
+        stats_h0 = np.load(cash_file)
+    else:
+        print('Simulate and save to {}'.format(cash_file))
+        stats_h0 = np.zeros(n_iter)
+        for k in range(n_iter):
+            if d is None:
+                z_star = np.random.normal(size=n)
+            else:
+                z_star = np.random.standard_t(d, size=n)
+            stats_h0[k] = eval_fan98_stat(z_star, d, transform)
+        if not os.path.exists(cash_dir):
+            os.makedirs(cash_dir)
+        np.save(cash_file, stats_h0)
+    return stats_h0
+
+
+def get_p_val(val, h0_distribution):
+    p_val = np.sum(val < h0_distribution)/h0_distribution.shape[0]
+    return p_val
+
+if __name__ == '__main__':
+    n = 20
+
+    import pandas as pd
+    paper_table = pd.read_csv('release/stats/upperquartile.csv', sep=';').values
+    p_vals = paper_table[0, 1:]
+    stats = paper_table[paper_table[:, 0] == n, 1:][0]
+
+
+    d = None
+    stats_h0 = simulate_h0_distribution(n, d, transform='legendre')
+
+
+    levels = np.zeros_like(p_vals)
+    for k, p_val in enumerate(p_vals):
+        levels[k] = np.quantile(stats_h0, 1 - p_val)
+
+    print(' '.join(['{:.2f}'.format(p*100) for p in p_vals]))
+    print(' '.join(['{:.2f}'.format(level) for level in levels]))
+    print(' '.join(['{:.2f}'.format(level) for level in stats]))
+    print(' '.join(['{:.2f}'.format(level) for level in (stats-levels)/stats*100]))
+
